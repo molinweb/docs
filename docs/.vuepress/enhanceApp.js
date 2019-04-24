e73d2771005b21ceb8fefaa27dbac2959a408595
7revor <7071242@qq.com>
2019-04-24 18:26:30 +0800

    change setting

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,3 @@
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 export default ({
                   Vue, // VuePress 正在使用的 Vue 构造函数
                   options, // 附加到根实例的一些选项
@@ -7,5 +5,4 @@ export default ({
                   siteData // 站点元数据
                 }) => {
   // ...做一些其他的应用级别的优化
-  Vue.use(Element)
 }
