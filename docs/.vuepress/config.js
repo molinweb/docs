8bf282589626ff99ecd1771cbfe58c640fcb85e7
7revor <7071242@qq.com>
2019-04-27 11:50:59 +0800

    aa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,56 +1,66 @@
 module.exports = {
-	title: '7revor',
-	description: '可爱又迷人的反派角色',
-  base:'/docs/',
+  title: '7revor',
+  description: '可爱又迷人的反派角色',
+  base: '/docs/',
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ['link', {
+      rel: 'icon',
+      href: '/favicon.ico'
+    }]
   ],
   markdown: {
     lineNumbers: false
   },
   themeConfig: {
     lastUpdated: 'Last Updated', // string | boolean
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'React', link: '/React/' },
-      { text: 'Documents', link: '/Notes/' },
-      { text: 'VuePress',link: '/Tutorial/'},
-    ],
-    displayAllHeaders: false,//显示所有页面的标题链接
+    nav: [{
+      text: 'Home',
+      link: '/'
+    }, {
+      text: 'React',
+      link: '/React/'
+    }, {
+      text: 'Documents',
+      link: '/Notes/'
+    }, {
+      text: 'VuePress',
+      link: '/Tutorial/'
+    }, ],
+    displayAllHeaders: false, //显示所有页面的标题链接
     sidebarDepth: 2,
     sidebar: {
       '/React/': [
-        ['Animated','React动画指南'],
-        ['ActionSheet','动作菜单组件的封装'],
-        ['Dialog','动态弹窗'],
-        ['TabBar','导航栏组件'],
-        ['titleWrapper','简单实用的高阶组件'],
-        ['closeKeyBoard','QAP自动关闭键盘'],
-        ['heightProvider','动态组件自适应键盘高度'],
-        ['listView','ListView长列表封装'],
-        ['cardHoc','卡片弹窗HOC']
+        ['Animated', 'React动画指南'],
+        ['ActionSheet', '动作菜单组件的封装'],
+        ['Dialog', '动态弹窗'],
+        ['TabBar', '导航栏组件'],
+        ['titleWrapper', '简单实用的高阶组件'],
+        ['closeKeyBoard', 'QAP自动关闭键盘'],
+        ['heightProvider', '动态组件自适应键盘高度'],
+        ['listView', 'ListView长列表封装'],
+        ['cardHoc', '卡片弹窗HOC']
       ],
-      '/Tutorial/':[
-        ['','VuePress']
+      '/Tutorial/': [
+        ['', 'VuePress']
       ],
       '/Notes/': [
-        ['publish','发布宝贝'],
-        ['goodsProps','宝贝属性编辑'],
+        ['publish', '发布宝贝'],
+        ['goodsProps', '宝贝属性详解'],
       ],
       /**
-			 *  // fallback,确保 fallback 侧边栏被最后定义。VuePress 会按顺序遍历侧边栏配置来寻找匹配的配置。
+       *  // fallback,确保 fallback 侧边栏被最后定义。VuePress 会按顺序遍历侧边栏配置来寻找匹配的配置。
        */
       '/': [
-        '',        /* / */
-    /*    'contact', /!* /contact.html *!/
-        'about'    /!* /about.html *!/*/
+        '', /* / */
+        /*    'contact', /!* /contact.html *!/
+            'about'    /!* /about.html *!/*/
       ]
     }
 
     /**
-		 * 侧边栏分组
+     * 侧边栏分组
      */
-		/*[
+    /*[
       {
         title: '笔记',
         collapsable: true,//侧边栏的每个子组默认是可折叠的，你可以设置 collapsable: false 来让一个组永远都是展开状态。
